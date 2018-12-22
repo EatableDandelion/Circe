4f10aa48379981532d463f9e3a0fac8aa79f30e0
EatableDandelion <mplasseraud@gmail.com>
2018-12-22 11:17:19 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
 #include "Circe.h"
 #include <memory>
+#include <iostream>
+#include <string>
+
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 using namespace Circe;
@@ -8,8 +12,21 @@ using namespace Circe;
 
 int main()
 {
-
 	
+	ifstream file;
+	file.open("TestShader.txt");
+	string uniformName = "uniform";
+	size_t uniformLocation = file.find()
+	std::string line;
+		Shader tmp;
+		if(std::getline(stream, line))
+		{
+			stringstream iss(line);
+			string s;
+			getline(iss, s, 'vec3');
+			cout << s << endl;
+		}
+	while()
 	
 	return 0;
 }
